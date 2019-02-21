e7c1cee7272a49e99d10d3c6208ead837de15145
dflores234 <31484736+dflores234@users.noreply.github.com>
2019-02-21 09:47:31 -0600

    Update listarCarreras.php

--- php/listarCarreras.php
+++ php/listarCarreras.php
@@ -2,7 +2,7 @@
 	require 'db_conection.php';
 
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-	{g
+	{
 		$stmt = $pdo->prepare('SELECT id_carrera, nombre_carrera FROM carrera'); 
 		$stmt->execute();
 		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
