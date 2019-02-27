1565781265f69f4008253ca991db8cc167735ece
dflores234 <31484736+dflores234@users.noreply.github.com>
2019-02-27 09:18:16 -0600

    Agregar php

--- php/usuario.class.php
+++ php/usuario.class.php
@@ -13,10 +13,28 @@
 		}
 
 
-		public function CrearUsuario()
+		public function CrearUsuario($nombre,$apP,$apM,$correo,$numero_contacto,$contrasena,$semestre,$status,$id_carrera)
 		{
-			
+			if (!$this->existeUsuario($correo)) 
+			{
+				# code...
+			}else
+			{
+
+			}
 		}
+
+
+		/*
+		* Metodo que nos indica si un usuario ya se encuentra registrado
+		*/
+		public function existeUsuario($email)
+	    {
+	    	$stmt = $this->connX->prepare("SELECT id_alumno FROM alumno WHERE correo_electronico = :email");
+	    	$stmt->bindParam(':email',$email);
+	    	$stmt->execute();
+			return $stmt->rowCount() > 0;
+	    }
 	}
 
 
