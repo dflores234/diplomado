20ad17446ccd2edfbe1bde99ffaedec77d2cc4b7
dflores234 <31484736+dflores234@users.noreply.github.com>
2019-02-27 09:00:45 -0600

    Php agregados

---
+++ php/usuario.class.php
@@ -0,0 +1,28 @@
+<?php 
+	require 'db_conection.php';
+
+	/**
+	 * 
+	 */
+	class Usuario
+	{
+		private $conectar;
+		function __construct(argument)
+		{
+			$this->conectar = $pdo;
+		}
+
+
+		public function CrearUsuario()
+		{
+			
+		}
+	}
+
+
+
+
+
+
+
+ ?>
