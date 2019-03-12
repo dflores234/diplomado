bc90510ea064110458bb52a84f84ba18e42c10ef
dflores234 <31484736+dflores234@users.noreply.github.com>
2019-03-12 15:41:41 -0500

    Update sesion.class.php

--- php/sesion.class.php
+++ php/sesion.class.php
@@ -4,11 +4,35 @@
 class Session 
 {
 
-    
+    //Inicia una session sino existe
+    public function __construct()
+    {
+        if(!isset($_SESSION))
+        {
+            session_start();
+        }
+    }
 
+    //Setea un valor en una session
+    public function setSession($key, $value) 
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    //Obtiene el valor de una sesión por medio del nombre.
+    public function getSession($key) 
+    {
+        $result = NULL;
+        if ($this->checkSession($key)) 
+        {
+            $result = $_SESSION[$key];
+        }
+        
+        return $result;
+    }
 
 	//Verifica si una clase está seteada por su nombre.
-    public static function check($key) 
+    public function checkSession($key) 
     {
         $result = FALSE;
         if (isset($_SESSION[$key])) 
@@ -18,47 +42,15 @@ class Session
         
         return $result;
     }
-    //Obtiene el valor de una sesión por medio del nombre.
-    public static function get($key) 
+
+    public function __destruct()
     {
-        $result = NULL;
-        if (self::check($key)) 
-        {
-            $result = $_SESSION[$key];
-        }
-        
-        return $result;
-    }
-    //Elimina la sesión luego de obtener su valor.
-    public static function getNdelete($key) {
-        $result = self::get($key);
-        self::delete($key);
-        return $result;
-    }
-    //Setea una nueva sesión.
-    public static function set($key, $value) {
-        $_SESSION[$key] = $value;
-    }
-    //Elimina por completo una sesión.
-    public static function delete($key) {
-        if (self::check($key)) {
-            unset($_SESSION[$key]);
+        if(isset($_SESSION))
+        { 
+            session_destroy();
         }
     }
 }
-/*
-	
-	
-
-<?php
-Session::set('Saludo', 'Hola mundo');  // Seteamos una sesión
-if (Session::check('Saludo')) {
-    echo Session::get('Saludo'); // "Hola mundo"
-}
-Session::getNdelete('Saludo');
-echo Session::get('Saludo'); //    (no se obtuvo nada)
+    //https://phpocean.com/tutorials/back-end/learn-how-to-use-php-sessions/56`
+    //https://www.tutorialspoint.com/php/index.htm
 ?>
-*/
-?>
-
-
