9a05035d78253622b9e06ad5ec70549a7d70eb89
alexhdz13 <47537808+alexhdz13@users.noreply.github.com>
2019-03-15 12:23:36 -0500

    r

--- js/validar.js
+++ js/validar.js
@@ -14,12 +14,28 @@ function validar(e){
     	return false;
     }
 }
+function validar(s){
+  if ($('#contraseña').val()!=$('#ccontraseña').val()) {
+      swal({
+        title: "La contraseñas no coinciden",
+          text: "por favor vuleva a introducir las contraseñas",
+          type: "warning",
+          /*showCancelButton: true,*/
+          confirmButtonColor: '#3085d6',
+          /*cancelButtonColor: '#d33',*/
+          confirmButtonText: 'Regresar',
+          /*cancelButtonText: "Cancelar"*/
+        });
+      e.preventDefault();
+      return false;
+    }
+}
 
 $(function() {
       $('#form-registrar').submit(function(e) {
         validar(e);
       });
-      $('#form-cambios').submit(function(e) {
-        validar(e);
+      $('#form-cambios').submit(function(s) {
+        validar(s);
       });
     });
--- participante_index.php
+++ participante_index.php
@@ -319,6 +319,83 @@
      </div>
    </div>
   </div>
+  <!-- Footer -->
+    <footer class="page-footer font-small blue pt-4 bg-dark">
+
+        <!-- Footer Links -->
+        <div class="container-fluid text-center text-md-left">
+
+          <!-- Cuadricula row -->
+          <div class="row">
+
+            <!-- Cuadricula column -->
+            <div class="col-md-6 mt-md-0 mt-3">
+              <!-- Contenido -->
+              <h5 class="text-uppercase text-light">
+                Centro de Innovación Socioecónomica y Tecnológica
+              </h5>
+              <div class="infof">
+                
+                  <img src="img/phone.svg" width="20"> +52 (867) 712-0327 <br>
+                  <img src="img/pin.svg" width="20"> contacto@centroist.org
+                
+              </div>
+                
+                <div class="infod">
+                    Av. Luis Donaldo Colosio Km. 2.5,<br>
+                    Valle real<br>
+                    Nuevo Laredo, Tamaulipas. México. <br>
+                    CP: 88000<br>
+                    © 2019 Copyright | Centro de Innovación Socioecónomica y Tecnológica
+                  
+                </div>
+            </div>
+            <!-- Cudri column -->
+
+            <hr class="clearfix w-100 d-md-none pb-3">
+
+            <!-- Cuadri column -->
+            <div class="" style="display: inline-block;">
+
+                <!-- Links -->
+                <!--<h5 class="text-uppercase text-light">Siguenos</h5>-->
+
+                <ul class="list-unstyled ">
+                  <li>
+                    <a href="https://www.facebook.com/centroist/"><img src="img/facebook.svg" width="35"></a>
+                  </li>
+                  <br>
+                  <li>
+                    <a href="http://centroist.org"><img src="img/istblanco.png" width="35" height="30"></a>
+                  </li>
+                </ul>
+
+              </div>
+              <!-- Cuadri column -->
+
+              <!-- Cuadri column -->
+              <div class="col-md-3 mb-md-0 mb-3">
+               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3541.4031901302506!2d-99.50885468520613!3d27.425542682909526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x866118b8bdd010a5%3A0x1bbdad4f53dc3ec1!2sCentro+Cultural+Nuevo+Laredo!5e0!3m2!1ses-419!2smx!4v1551718555256" width="500" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div>
+              <!-- Cuadri column -->
+
+          </div>
+          <!-- Cuadri row -->
+
+        </div>
+        <!-- Footer Links -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3 bg-light">
+          <img src="img/logo4.png" width="110" id="logo">
+          <img src="img/Logo CENTRO IST BLANCO-05[1393].png" width="180" height="50" id="logo" style="background: lightblue; border-radius: 6px;">
+          <img src="img/logo3.png" width="180" id="logo">
+          <img src="img/itnl.gif" width="70" id="logo" style="">
+          <img src="img/logo2.png" width="150" id="logo" style="margin-left: ">
+        </div>
+        <!-- Copyright -->
+
+    </footer>
     </body>
   </html>
 <!--  JavaScript -->
