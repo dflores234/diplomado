3bc9a28a8d95fa47cda4bb7675be0c5df0fa3354
dflores234 <31484736+dflores234@users.noreply.github.com>
2019-04-23 23:33:35 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,9 @@
 <?php 
-
+  //http://velozityweb.com/blog/php/como-subir-imagenes-o-fotos-al-servidor-con-php/#sthash.Dwe6S2jE.dpbs
+//https://www.w3schools.com/php/php_file_upload.asp
+//https://codigosdeprogramacion.com/2017/03/02/curso-de-php-y-mysql-21-recuperar-contrasena-por-email/
+//https://www.todoexpertos.com/preguntas/6bdxo3eh7n6q5ky6/como-puedo-subir-una-imagen-al-servidor-y-guardar-la-ruta-en-la-base-de-datos-con-php-y-mysql-he-probado-con-mas-de-5-videos
+//https://manuais.iessanclemente.net/index.php/Almacenamiento_de_imágenes_en_bases_de_datos_con_PHP
 ?>
 <!DOCTYPE html>
 	<html>
@@ -20,10 +24,6 @@
 	  				<a class="navbar-brand" href="index.html">
 	  					<img src="img/Diplomado3 - copia.png" width="160" height="63" class="d-inline-block align-top" alt="">
 	  				</a> 
-	  				<!--button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	    				<span class="navbar-toggler-icon">
-	    				</span>
-	  				</button-->
 				</nav>
     		</div>  
     		<!--Fin menu-->
@@ -63,82 +63,7 @@
 					</div>
 				</div>
 			</div>
-			
-			       <!-- Footer -->
-<footer class="page-footer font-small blue pt-4 bg-dark">
-
-    <!-- Footer Links -->
-    <div class="container-fluid text-center text-md-left">
-
-      <!-- Cuadricula row -->
-      <div class="row">
-
-        <!-- Cuadricula column -->
-        <div class="col-md-6 mt-md-0 mt-3">
-          <!-- Contenido -->
-          <h5 class="text-uppercase text-light">
-            Centro de Innovación Socioecónomica y Tecnológica
-          </h5>
-          <div class="">
-            <p class="text-light">
-              <i class="fas fa-phone"></i> +52 (867) 712-0327 <br>
-              <i class="fas fa-envelope-open"></i> contacto@centroist.org
-            </p>
-          </div>
-            
-            <div class="infod">
-              <p class="text-light">
-               Av. Luis Donaldo Colosio Km. 2.5,<br>
-               Valle real<br>
-        			 Nuevo Laredo, Tamaulipas. México. <br>
-        			 CP: 88000 <br>
-                © 2019 Copyright | Centro de Innovación Socioecónomica y Tecnológica
-              </p>
-            </div>
-        </div>
-        <!-- Cudri column -->
-
-        <hr class="clearfix w-100 d-md-none pb-3">
-
-        <!-- Cuadri column -->
-        <div class="" style="display: inline-block;">
-
-            <!-- Links -->
-            <!--<h5 class="text-uppercase text-light">Siguenos</h5>-->
-
-            <ul class="list-unstyled ">
-              <li>
-                <a href="https://www.facebook.com/centroist/"><img src="img/facebook.svg" width="35"></a>
-              </li> <br>
-              <li>
-                <a href="http://centroist.org"><img src="img/istblanco.png" width="35" height="30"></a>
-              </li>
-              
-            </ul>
-
-          </div>
-          <!-- Cuadri column -->
-
-          <!-- Cuadri column -->
-          <div class="col-md-3 mb-md-0 mb-3">
-           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3541.4031901302506!2d-99.50885468520613!3d27.425542682909526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x866118b8bdd010a5%3A0x1bbdad4f53dc3ec1!2sCentro+Cultural+Nuevo+Laredo!5e0!3m2!1ses-419!2smx!4v1551718555256" width="500" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
-          </div>
-          <!-- Cuadri column -->
-
-      </div>
-    </div>
-    <!-- Copyright -->
-    <div class="footer-copyright text-center py-3 bg-light">
-    	<img src="img/logo4.png" width="110" id="logo">
-      <img src="img/Logo CENTRO IST BLANCO-05[1393].png" width="180" height="50" id="logo">
-      <img src="img/logo3.png" width="180" id="logo">
-      <img src="img/itnl.gif" width="70" id="logo" style="">
-      <img src="img/logo2.png" width="150" id="logo" style="margin-left: ">
-    </div>
-    <!-- Copyright -->
-
-</footer>
-  <!-- Footer -->
+		
 
   <!-- Modal Retroalimentación-->
   <div class="modal fade" id="modalRetro" tabindex="-1" role="dialog" aria-hidden="true">
@@ -190,7 +115,81 @@
     </div>
   </div>
 
+           <!-- Footer -->
+<footer class="page-footer font-small blue pt-4 bg-dark">
 
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+
+      <!-- Cuadricula row -->
+      <div class="row">
+
+        <!-- Cuadricula column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+          <!-- Contenido -->
+          <h5 class="text-uppercase text-light">
+            Centro de Innovación Socioecónomica y Tecnológica
+          </h5>
+          <div class="">
+            <p class="text-light">
+              <i class="fas fa-phone"></i> +52 (867) 712-0327 <br>
+              <i class="fas fa-envelope-open"></i> contacto@centroist.org
+            </p>
+          </div>
+            
+            <div class="infod">
+              <p class="text-light">
+               Av. Luis Donaldo Colosio Km. 2.5,<br>
+               Valle real<br>
+               Nuevo Laredo, Tamaulipas. México. <br>
+               CP: 88000 <br>
+                © 2019 Copyright | Centro de Innovación Socioecónomica y Tecnológica
+              </p>
+            </div>
+        </div>
+        <!-- Cudri column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <!-- Cuadri column -->
+        <div class="" style="display: inline-block;">
+
+            <!-- Links -->
+            <!--<h5 class="text-uppercase text-light">Siguenos</h5>-->
+
+            <ul class="list-unstyled ">
+              <li>
+                <a href="https://www.facebook.com/centroist/"><img src="img/facebook.svg" width="35"></a>
+              </li> <br>
+              <li>
+                <a href="http://centroist.org"><img src="img/istblanco.png" width="35" height="30"></a>
+              </li>
+              
+            </ul>
+
+          </div>
+          <!-- Cuadri column -->
+
+          <!-- Cuadri column -->
+          <div class="col-md-3 mb-md-0 mb-3">
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3541.4031901302506!2d-99.50885468520613!3d27.425542682909526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x866118b8bdd010a5%3A0x1bbdad4f53dc3ec1!2sCentro+Cultural+Nuevo+Laredo!5e0!3m2!1ses-419!2smx!4v1551718555256" width="500" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+          <!-- Cuadri column -->
+
+      </div>
+    </div>
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3 bg-light">
+      <img src="img/logo4.png" width="110" id="logo">
+      <img src="img/Logo CENTRO IST BLANCO-05[1393].png" width="180" height="50" id="logo">
+      <img src="img/logo3.png" width="180" id="logo">
+      <img src="img/itnl.gif" width="70" id="logo" style="">
+      <img src="img/logo2.png" width="150" id="logo" style="margin-left: ">
+    </div>
+    <!-- Copyright -->
+
+</footer>
+  <!-- Footer -->
 
 
 
@@ -208,7 +207,11 @@
       {
           
         $('#btnLogin').click(function()
+<<<<<<< HEAD
         {
+=======
+        {/*
+>>>>>>> e9c34541b9c034964337e91a10ccf35291dc7614
             $.ajax
                   ({
                     url: 'php/usuario.controller.php',
@@ -237,7 +240,11 @@
                     {
                       $('#btnLogin').html('').append('<i class="fas fa-sign-out-alt"></i>Ingresar');
                     }
+<<<<<<< HEAD
                   });
+=======
+                  });*/
+>>>>>>> e9c34541b9c034964337e91a10ccf35291dc7614
         });
 
         $('#btnRecuperarContrasena').click(function()
@@ -249,7 +256,31 @@
             }else
             {
               $('#rpemail').removeClass('border border-danger');
-              //AJAX
+               $.ajax
+                  ({
+                    url: 'php/usuario.controller.php',
+                    type: 'POST',
+                    dataType: 'JSON',
+                    data: {opcion: 'enviar',correo: $('#correo').val()},
+                    beforeSend: function()
+                    {
+                        $('#btnRecuperarContrasena').html('').append('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Por favor espere...');
+                    },
+                    success: function(data)
+                    {
+                      if(data.status == 'ok')
+                      {
+                        console.log(data);
+                      }else
+                      {
+                        console.log(data);
+                      }
+                    },
+                    error: function(error)
+                    {
+                      $('#btnRecuperarContrasena').html('').append('<i class="fas fa-sign-out-alt"></i>Ingresar');
+                    }
+                  });
             }
         });
 
