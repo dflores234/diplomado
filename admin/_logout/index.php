61d21c6b1340308959d70d3f5c8f1dda5b6d1577
dflores234 <31484736+dflores234@users.noreply.github.com>
2019-04-30 12:49:07 -0500

    :)

---
+++ admin/_logout/index.php
@@ -0,0 +1,9 @@
+<?php
+echo "<link rel='stylesheet' href='../../css/bootstrap.min.css'>";
+	echo "<div class='alert alert-info' role='alert'><strong>Cerrando sesión, por favor espere</strong>
+			<div class='spinner-grow spinner-border-sm text-info' role='status'></div>
+			<div class='spinner-grow spinner-border-sm text-info' role='status'></div>
+			<div class='spinner-grow spinner-border-sm text-info' role='status'></div>
+		</div>"; 
+header("Refresh:3; URL= ..\..\index.php");
+?>
