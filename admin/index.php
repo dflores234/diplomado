8e11eb933a31da2a9548708b675da46fa37551ab
dflores234 <31484736+dflores234@users.noreply.github.com>
2019-04-30 13:13:11 -0500

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,75 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
-      		<meta charset="utf-8">
+          <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="../css/panel.css">
         <link rel="shortcut icon" href="../img/istblanco.png">
         
-	<title>Panel de Administrador</title>
+  <title>Panel de Administrador</title>
 </head>
 <body>
-<<<<<<< HEAD
+  <!--Navbar-->
+  <div class="container-fluid fixed-top text-center"  style="background-color: #23AAE1">
+          <nav class="navbar navbar-expand-lg navbar-light container" style="background-color: #23AAE1">
+            <a class="navbar-brand" href="index.php">
+              <img src="../img/Diplomado3 - copia.png" width="160" height="63" class="d-inline-block align-top" alt="">
+            </a> 
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon">
+              </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <div class="navbar-nav ml-auto">
+                <div>Bienvenido administrador</div>
+                  <div>
+                    <a href="http://fakeuser:fakepassword@localhost/diplomado/admin/_logout/" class="btn btn-light">Salir</a>
+                  </div>
+              </div>
+            </div>
+        </nav>
+        </div>
 
-<!-- NAVBAR -->
-<div class="container-fluid fixed-top text-center"  style="background-color: #23AAE1">
-    			<nav class="navbar navbar-expand-lg navbar-light container" style="background-color: #23AAE1">
-	  				<a class="navbar-brand" href="../index.php">
-=======
-	<!--Navbar-->
-		<div class="container-fluid fixed-top text-center"  style="background-color: #23AAE1">
-    			<nav class="navbar navbar-expand-lg navbar-light container" style="background-color: #23AAE1">
-	  				<a class="navbar-brand" href="index.php">
->>>>>>> 088ba4e6794d630e86177935cd326ef7fb33e4aa
-	  					<img src="../img/Diplomado3 - copia.png" width="160" height="63" class="d-inline-block align-top" alt="">
-	  				</a> 
-	  				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	    				<span class="navbar-toggler-icon">
-	    				</span>
-	  				</button>
-	  				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-	    				<div class="navbar-nav ml-auto">
-<<<<<<< HEAD
-	      					<div>
-	      						<a href="http://fakeuser:fakepassword@localhost/diplomado/admin/_logout/" class="btn btn-light">Salir</a>
-	      					</div>
-	    				</div>
-	  				</div>
-				</nav>
-    		</div>
-
-
-	<!---SASASA-->
+  <!---SASASA-->
 <br>
 <br>
 <br>
 <br>
 <div class="container">
-  <h2>Administrador</h2>
-=======
-	    					<div>
-	    						<p class="btn btn-default"> Bienvenido Administrador</p>
-	    					</div>
-	      					<div>
-	      						<a href="http://fakeuser:fakepassword@localhost:8080/diplomado/admin/_logout/" class="btn btn-group">Salir</a>
-	      					</div>
-	      				</div>
-	    			</div>
-				</nav>
-    	</div>	
-
-
-
-
-
-
-	<!---SASASA-->
-<br><br>
-<br>
-<br>
-<div class="container">
->>>>>>> 088ba4e6794d630e86177935cd326ef7fb33e4aa
   <ul class="nav nav-tabs" id="myTab" role="tablist">
   <li class="nav-item">
     <a class="nav-link active" data-toggle="tab" href="#home" role="tab" aria-controls="home">Home</a>
@@ -88,10 +55,7 @@
 <div class="tab-content">
   <div class="tab-pane active" id="home" role="tabpanel">
   <br>
-<<<<<<< HEAD
-  Bienvenido administrador
-=======
->>>>>>> 088ba4e6794d630e86177935cd326ef7fb33e4aa
+
 </div>
   <div class="tab-pane" id="profile" role="tabpanel">
       <table class="table table-bordered">
@@ -127,11 +91,7 @@
                       <div class="form-group">
                                 <label class="col-md control-label"></label>
                                 <div class="col-md-2" >
-<<<<<<< HEAD
                                   <input type="submit" class="btn btn-primary"  id="activar" value="Activar">
-=======
-                                  <input type="submit" class="btn btn-primary float-right"  id="activar" value="Activar">
->>>>>>> 088ba4e6794d630e86177935cd326ef7fb33e4aa
                                   <span></span>
                                 </div>
                       </div>
@@ -204,16 +164,9 @@
                     <input class="form-control" id="txttelefono" type="date">
                   </div>
                 </div>
-<<<<<<< HEAD
-                <div class="form-group" style="display: inline-block;">
-                  <label class="col-lg-12 control-label">Modificar fecha:</label>
+                 <div class="form-group" style="display: inline-block;">
                   <div class="col-lg-12">
-                    <input class="btn btn-primary" id="btnModificarModulo" type="button" value="Cambiar Fecha">
-=======
-                <div class="form-group" style="display: inline-block;">                  
-                  <div class="col-lg-12">
-                    <input class="btn btn-info" id="btnModificarFecha" type="button" value="Modificar fecha">
->>>>>>> 088ba4e6794d630e86177935cd326ef7fb33e4aa
+                    <input class="btn btn-success" id="btnModificarFecha" type="button" value="Cambiar fecha">
                   </div>
                 </div>
   </div>
@@ -230,7 +183,7 @@
 </body>
 </html>
 <script type="text/javascript" src="../js/jquery.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
