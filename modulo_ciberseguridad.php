13809b3031b8c58809bab759ae06ce02a8028bb4
alexhdz13 <47537808+alexhdz13@users.noreply.github.com>
2019-05-08 11:08:50 -0500

    s

--- modulo_ciberseguridad.php
+++ modulo_ciberseguridad.php
@@ -63,43 +63,7 @@
               <div class="row">
                 <div class="col-lg-12">
                   <!--Menu-->
-                    <div class="container-fluid text-right" style="background-color: #23AAE1">
-                      <nav class="navbar navbar-expand-lg navbar-light container" style="background-color: #23AAE1">
-                        <a class="navbar-brand" href="participante_index.php">
-                            <img src="img/Diplomado3 - copia.png" width="160" height="63" class="d-inline-block align-top" alt="">
-                        </a> 
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                          <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <div class="navbar-nav ml-auto">
-                                <div>
-                                  <a href="#menu-toggle" class="btn btn-default chatb" id="menu-toggle"> 
-                                    <img src="img/group.svg" width="25" height="25" class="d-inline-block align-top" alt="">
-                                </a>
-                                </div>
-                                <div class="dropdown pmd-dropdown pmd-user-info ml-auto">
-                                    <a href="javascript:void(0);" class="btn-user dropdown-toggle media align-items-center" data-toggle="dropdown" data-sidebar="true" aria-expanded="false">
-                                        <img class="mr-2 rounded-circle" src='<?php echo $avatar; ?>' width="40" height="40" alt="avatar">
-                                        <div class="media-body" id="userc">
-                                          <?php echo $nombre; ?>
-                                        </div>
-                                        <i class="material-icons md-light ml-2 pmd-sm"></i>
-                                    </a>
-                                    <ul class="dropdown-menu dropdown-menu-right" role="menu">
-                                        <a class="dropdown-item" href="editar_perfil.php">
-                                            Editar Perfil
-                                        </a>
-                                        <a class="dropdown-item" href="php/destroysession.php">
-                                            Cerrar sesión
-                                        </a>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </nav>
-                </div>  
+                    <?php include "header2.php" ?>
                 <!--Fin menu-->
                 <section class="main-section">
     	             <div style="width: 500px;margin: 0 auto;">
