0d63ec3db525152c6acfed24540984a523202f82
alexhdz13 <47537808+alexhdz13@users.noreply.github.com>
2019-05-09 11:54:35 -0500

    maestros

--- home_maestros.php
+++ home_maestros.php
@@ -31,7 +31,7 @@
 								    <span class="sr-only">Toggle Dropdown</span>
 								  	</button>
 								  	<div class="dropdown-menu">
-								    	<a class="dropdown-item" href="http://fakeuser:fakepassword@localhost:8080/diplomado/admin/_logout/" class="btn btn-outline-dark" >Salir</a>
+								    	<a class="dropdown-item" href="#" class="btn btn-outline-dark" >Salir</a>
 								  	</div>
 							</div>
 		                  
@@ -58,6 +58,100 @@
        					<div style="width: 200px;margin: 0 auto;">
     		               <h1 style="font-size: 30px"> <img src="img/code.svg" width="40">PYTHON </h1>
     	             	</div>
+    	             	<div id="accordion" role="tablist">
+                      <div class="card">
+                        <div class="card-header bg-white" role="tab" id="headingOne">
+                          <h5 class="mb-0">
+                            <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                              Introducción a python
+                            </a>
+                          </h5>
+                        </div>
+                        <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" >
+                          <div class="card-body bg-light">
+                              <div class="contanier">
+                                <div class="row">
+                                  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6"><br> 
+                                    <iframe src="https://onedrive.live.com/embed?cid=B58A0C42EAF6FEE8&amp;resid=B58A0C42EAF6FEE8%21116&amp;authkey=ADddGjuHiXXNY8w&amp;em=2&amp;wdAr=1.3333333333333333" width="500" height="400" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
+                                  </div>
+                                  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                      <h3>Introducción a Python</h3><hr style="background-color: black">
+                                          <ul align="left" style="font-size: 14px;">
+                                             <li>¿Qué es Python?</li>
+                                            <li>Ventajas de python </li>
+                                            <li>Entorno de python</li>
+                                            <li>Declaracíon de variables</li>
+                                            <li>Imprimir mensajes</li>
+                                            <li>Ingresar datos</li>
+                                            <li>Operaciones matematicas y funciones matematicas</li>
+                                            <li>Importar librerias</li>                              
+                                          </ul>
+                                  </div>
+                                </div>
+                              </div> 
+                          </div>
+                        </div>
+                    </div>
+  
+                      <div class="card">
+                        <div class="card-header bg-white" role="tab" id="headingTwo">
+                          <h5 class="mb-0">
+                            <a data-toggle="collapse" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                              Sentencias en python
+                            </a>
+                          </h5>
+                        </div>
+                        <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion" >
+                          <div class="card-body bg-light">
+                              <div class="contanier">
+                                <div class="row">
+                                  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6"><br> 
+                                    <iframe src="https://onedrive.live.com/embed?cid=B58A0C42EAF6FEE8&amp;resid=B58A0C42EAF6FEE8%21117&amp;authkey=AMEzBstc55hAcco&amp;em=2&amp;wdAr=1.7777777777777777" width="500" height="320" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
+                                  </div>
+                                  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                      <h3>Sentencias en python</h3><hr style="background-color: black">
+                                          <ul align="left" style="font-size: 14px;">
+                                <li>Sentencia 'if'</li>
+                                <li>Operadores condicionales </li>
+                                <li>Errores comunes</li>
+                                <li>Sentencia 'elif'</li>
+                                <li>Misceláneo</li>
+                                <li>Conteo</li>
+                                <li>Suma</li>
+                                <li>Intercambio</li>
+                                <li>Banderas</li>
+                                <li>Máximos y mínimos</li>
+                                <li>Comentarios</li>
+                              </ul>
+                                  </div>
+                                </div>
+                              </div> 
+                          </div>
+                        </div>
+                    </div>
+                      <div class="card">
+                        <div class="card-header bg-white" role="tab" id="headingThree">
+                          <h5 class="mb-0">
+                            <a data-toggle="collapse" href="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                              Material descargable
+                            </a>
+                          </h5>
+                        </div>
+                        <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion" >
+                          <div class="card-body bg-light">
+                              <div class="contanier">
+                                <div class="row">
+                                  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                    <a href="https://www.python.org/downloads/"  target="_blank" style="color:blue">Descarga python</a>
+                                  </div>
+                               
+                                </div>
+                              </div> 
+                          </div>
+                        </div>
+                    </div>
+              
+                    </div>
        				</div>
        				<div class="tab-pane" id="contenidoadd" role="tabpanel">
        					<br>
@@ -95,6 +189,8 @@
        		</div>
         </div>
         <!--Fin del contenido de home-->
+        <div style="margin-bottom: 200px"></div>
+        <?php include "footer.php" ?>
 </body>
 </html>
 
