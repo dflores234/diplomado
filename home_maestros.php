7d2d073043d2557a0fbfc64751866c5b76d40505
alexhdz13 <47537808+alexhdz13@users.noreply.github.com>
2019-05-09 11:46:59 -0500

    maestros

---
+++ home_maestros.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   		<link rel="stylesheet" type="text/css" href="css/home_maestro.css">
+        <link rel="shortcut icon" href="img/istblanco.png">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<title>Home maestros</title>
+</head>
+<body>
+<!--Navbar-->
+  		<div class="container-fluid fixed-top text-center"  style="background-color: #23AAE1">
+          	<nav class="navbar navbar-expand-lg navbar-light container" style="background-color: #23AAE1">
+	            <a class="navbar-brand" href="index.php">
+	              <img src="img/Diplomado3 - copia.png" width="160" height="63" class="d-inline-block align-top" alt="">
+	            </a> 
+	            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		              <span class="navbar-toggler-icon">
+		              </span>
+	            </button>
+	            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	              	<div class="navbar-nav ml-auto">
+		                <div>
+		                  	<p class="font-weight-bold text-light" style="margin-top: 5px;">Bienvenido:"Nombre del maestro"</p>
+		                </div>
+		                <div>
+		                	<div class="btn-group" style="">
+								  	<button style="color: white;margin-right: 20px" type="button" class="btn dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								    <span class="sr-only">Toggle Dropdown</span>
+								  	</button>
+								  	<div class="dropdown-menu">
+								    	<a class="dropdown-item" href="http://fakeuser:fakepassword@localhost:8080/diplomado/admin/_logout/" class="btn btn-outline-dark" >Salir</a>
+								  	</div>
+							</div>
+		                  
+		                </div>
+	              	</div>
+	            </div>
+        	</nav>
+        </div>
+        <!--Contenido del home-->
+        <div class="main-section">
+       		<div class="contanier">
+       			<ul class="nav nav-tabs" id="myTab" role="tablist" >
+       				<li class="nav-item">
+       					<a class="nav-link active" data-toggle="tab" href="#contenido" role="tab" aria-controls="contenido">Contenido del modulo</a>
+       				</li>
+       				<li>
+       					<a class="nav-link" data-toggle="tab" href="#contenidoadd" role="tab" aria-controls="contenidoadd">Agregar contenido al modulo</a>
+       				</li>
+       			</ul>
+       			<!--Aqui va el contenido de las pesta;as-->
+       			<div class="tab-content">
+       				<div class="tab-pane active" id="contenido" role="tabpanel">
+       					<br>
+       					<div style="width: 200px;margin: 0 auto;">
+    		               <h1 style="font-size: 30px"> <img src="img/code.svg" width="40">PYTHON </h1>
+    	             	</div>
+       				</div>
+       				<div class="tab-pane" id="contenidoadd" role="tabpanel">
+       					<br>
+       					<form class="form-horizontal" role="form">
+       						 <div class="form-group">
+					              <label class="col-lg-3 control-label">Nombre del articulo:</label>
+					              <div class="col-lg-6">
+					              <input class="form-control" type="text" placeholder="Introduzca aquí el nombre del artículo" id="txtnombreart" required="required">
+					              </div>
+            				</div>
+            				<div class="form-group">
+							    <label class="col-lg-3 control-label" for="Descripcion">Descripción:</label>
+							    <div class="col-lg-6">
+							    	<textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+							    </div>  
+  							</div>
+  							<div class="form-group">
+					              <label class="col-lg-3 control-label">Link del artículo:</label>
+					              <div class="col-lg-6">
+					              <input class="form-control" type="text" placeholder="Introduzca aquí el link" id="txtlink" required="required">
+					              </div>
+            				</div>
+            				<div class="form-group">
+					                <label class="col-md-2 control-label"></label>
+					                <div class="col-md-6">
+					                  <button type="button" class="btn btn-info float-right" id="guardar"> 
+					                    <i class="fa fa-check-circle"></i>
+					                      Guardar
+					                  </button>
+					                </div>
+            				</div>
+       					</form>
+       				</div>
+       			</div>
+       		</div>
+        </div>
+        <!--Fin del contenido de home-->
+</body>
+</html>
+
+<!--Scripts-->
+<script type="text/javascript" src="js/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
