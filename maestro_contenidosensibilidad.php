b8ed397c79f7720f09e6b81e33a2c0c5bcf6e56c
alexhdz13 <47537808+alexhdz13@users.noreply.github.com>
2019-05-29 22:13:19 -0500

    minimos

---
+++ maestro_contenidosensibilidad.php
@@ -0,0 +1,44 @@
+<div style="width: 850px;margin: 0 auto;">
+      		        <h1><img src="img/Industry-4.0_01.png" width="70">Sensibilización y capacitación en I4.0 e IoT</h1>
+      	   </div>
+           <div id="accordion" role="tablist">
+              <div class="card">
+                        <div class="card-header bg-white" role="tab" id="headingOne">
+                            <h5 class="mb-0">
+                                <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                  Sensibilización en Industria 4.0
+                                </a>
+                            </h5>
+                        </div>
+                        <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" >
+                            <div class="card-body bg-light">
+                                <div class="contanier">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6"><br> 
+                                          <iframe  style="margin-left: 10px" src="https://onedrive.live.com/embed?cid=B58A0C42EAF6FEE8&amp;resid=B58A0C42EAF6FEE8%21120&amp;authkey=AH_efWPtOdDg_-g&amp;em=2&amp;wdAr=1.3333333333333333" width="440" height="350" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                            <h3>Sensibilización en Industria 4.0</h3><hr style="background-color: black">
+                                                <ul align="left" style="font-size: 14px;">
+                                                   <li>Evolución, Industria 4.0, Innovación, Industria y Tecnologia como concepto</li>
+                                                    <li>Avances tecnologicos en el tiempo </li>
+                                                    <li>Modelo XXI</li>
+                                                    <li>Causas del cambio-Sociedad 4.0 y Empresas 4.0</li>
+                                                    <li>Evolución, Revolución y Disrupción</li>
+                                                    <li>Innovar o morir</li>
+                                                    <li>Mega tendencias</li>
+                                                    <li>Nuevos productos y cadenas de valor</li> 
+                                                    <li>Nuevas habilidades y comportamiento de empleados</li>                             
+                                                    <li>Nuevo modelo de negocio</li>
+                                                    <li>Nuevos mercados</li>
+                                                    <li>Cambio cultural (Niños de la posguerra, Baby Boomers, Generación X, Milennials y Generación Z</li>
+                                                    <li>Nuevas tendencias de trabajo</li>
+                                                    <li>Herramientas de planeación</li>                             
+                                                </ul>
+                                        </div>
+                                    </div>
+                                </div> 
+                            </div>
+                        </div>
+                  </div>
+           </div>
---
+++ maestro_contenidovryar.php
@@ -0,0 +1,102 @@
+                <div style="width: 600px;margin: 0 auto; text-align: center;">
+                      <h1 style="font-size: 30px">
+                        <img src="img/AR.svg" width="50">Sistemas interactivos basados en visión para ayuda al operador (VR Y AR)
+                      </h1>
+                </div>  
+                <div id="accordion" role="tablist">
+                   <div class="card">
+                        <div class="card-header bg-white" role="tab" id="headingOne">
+                            <h5 class="mb-0">
+                                <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                  General Gamificación y Usabilidad
+                                </a>
+                            </h5>
+                        </div>
+                        <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" >
+                            <div class="card-body bg-light">
+                                <div class="contanier">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6"><br> 
+                                         <iframe src="https://onedrive.live.com/embed?cid=B58A0C42EAF6FEE8&amp;resid=B58A0C42EAF6FEE8%21139&amp;authkey=AAwA16BqvFcjkeg&amp;em=2&amp;wdAr=1.3333333333333333&amp;wdEaa=1" width="490px" height="406px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                            <h3>Introducción</h3><hr style="background-color: black">
+                                                <ul align="left" style="font-size: 14px;">
+                                                    <li>¿Por qué un bloque de GP?</li>
+                                                    <li>Objetivos</li>
+                                                    <li>Organización</li>                             
+                                                </ul>
+                                        </div>
+                                    </div>
+                                </div> 
+                            </div>
+                        </div>
+                  </div>
+                  <div class="card">
+                        <div class="card-header bg-white" role="tab" id="headingTwo">
+                            <h5 class="mb-0">
+                                <a data-toggle="collapse" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                  Realidad virtual 
+                                </a>
+                            </h5>
+                        </div>
+                        <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion" >
+                            <div class="card-body bg-light">
+                                <div class="contanier">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6"><br> 
+                                          <iframe src="https://onedrive.live.com/embed?cid=B58A0C42EAF6FEE8&amp;resid=B58A0C42EAF6FEE8%21140&amp;authkey=AE8k20ByNeIUvPs&amp;em=2&amp;wdAr=1.3333333333333333&amp;wdEaa=1" width="490px" height="406px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                            <h3>Módulo I</h3><hr style="background-color: black">
+                                                <ul align="left" style="font-size: 14px;">
+                                                    <li>Introducción a la Realidad Virtual</li>
+                                                    <li>Tipos Realidad Virtual (inmersiva, no inmersiva, etc.)​</li>
+                                                    <li>Análisis de dispositivos (comerciales actuales)​</li> 
+                                                    <li>Ejemplos de aplicación​</li>
+                                                    <li> Grado de madurez y retos​</li>
+                                                    <li>Cómo desarrollar activos/productos de RV, intro básica​</li>
+                                                </ul>
+                                        </div>
+                                    </div>
+                                </div> 
+                            </div>
+                        </div>
+                  </div>
+                  <div class="card">
+                        <div class="card-header bg-white" role="tab" id="headingThree">
+                            <h5 class="mb-0">
+                                <a data-toggle="collapse" href="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                  Realidad Aumentada 
+                                </a>
+                            </h5>
+                        </div>
+                        <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion" >
+                            <div class="card-body bg-light">
+                                <div class="contanier">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6"><br> 
+                                          <iframe src="https://onedrive.live.com/embed?cid=B58A0C42EAF6FEE8&amp;resid=B58A0C42EAF6FEE8%21141&amp;authkey=AEAA-mMo8t5Wzy0&amp;em=2&amp;wdAr=1.3333333333333333&amp;wdEaa=1" width="490px" height="406px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office Online</a>.</iframe>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                            <h3>Módulo II</h3><hr style="background-color: black">
+                                                <ul align="left" style="font-size: 14px;">
+                                                    <li>Introducción</li>
+                                                    <li>Línea del tiempo de la Realidad Aumentada​</li>
+                                                    <li>Grado de Madurez​</li> 
+                                                    <li>¿Cómo funciona?​</li>
+                                                    <li>Análisis de dispositivos​</li>
+                                                    <li>Sectores y Ejemplos de aplicación​</li>
+                                                    <li>Industria 4.0​</li>
+                                                    <li>Actualidad</li>
+                                                    <li>Conclusiones​</li>
+                                                    <li>Cómo desarrollar activos/ productos de RA​</li>
+                                                </ul>
+                                        </div>
+                                    </div>
+                                </div> 
+                            </div>
+                        </div>
+                  </div>
+                     
+                </div>  
